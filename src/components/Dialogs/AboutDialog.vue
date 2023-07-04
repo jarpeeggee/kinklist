243ee487133d2393b160b7c7203bd9706721a3fb
J <jarpeeggee@gmail.com>
2023-07-04 15:39:02 -0700

    Remove cloned user's settings

--- src/components/Dialogs/AboutDialog.vue
+++ src/components/Dialogs/AboutDialog.vue
@@ -9,16 +9,8 @@
         preferences. It is open source under the MIT license. You are thus free modify source
         code in any way you wish. Credit is appreciated but not required.
       </p>
-      <p>
-        If you have questions about kinklist, or suggestions, you can leave me a message on
-        Reddit (/u/Goctionni) or an issue on Github. I always enjoy hearing of people using
-        things I made, so if you're using Kinklist for your community- hearing about that is
-        also just lovely.
-      </p>
       <ul>
         <li><a href="https://github.com/Goctionni/kinklist-v2" target="_blank">Github</a></li>
-        <li><a href="https://www.reddit.com/user/Goctionni/" target="_blank">reddit.com/u/Goctionni</a></li>
-        <li>Discord: Goctionni#9137</li>
       </ul>
     </div>
   </Dialog>
